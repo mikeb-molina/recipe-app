85543df49e7235546ca4e3de35732d5ed0e41fb1
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-09-17 11:36:06 -0400

    added return to home link

--- A2_Recipe_App/src/recipes/urls.py
+++ A2_Recipe_App/src/recipes/urls.py
@@ -5,7 +5,7 @@ from .views import welcome, RecipeListView, RecipeDetailView
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', welcome),
+    path('', welcome, name='recipes_home'),
     path('list/', RecipeListView.as_view(), name='recipes_list'),
     path('list/<pk>', RecipeDetailView.as_view(), name='detail')
 ]
